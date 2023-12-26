13fd2ff17f49e428f879b97c5437873ae456b6d3
konjiii <kenji.opdam@gmail.com>
2023-12-26 15:39:43 +0100

    added handy function keybinds for lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -4,6 +4,13 @@ lsp_zero.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
 
   vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+  vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
+  vim.keymap.set("n", "<leader>gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "<leader>gr", function() vim.lsp.buf.references() end, opts)
+  vim.keymap.set("n", "<leader>si", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "<leader>sds", function() vim.lsp.buf.document_symbol() end, opts)
+  vim.keymap.set("n", "<leader>sws", function() vim.lsp.buf.workspace_symbol() end, opts)
+  vim.keymap.set("n", "<leader>se", function() vim.diagnostic.open_float() end, opts)
 end)
 
 require('mason').setup({})
