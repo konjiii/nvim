5fc837f953e89fd1feff51f4dc20565e14b86dae
konjiii <kenji.opdam@gmail.com>
2023-12-29 00:25:27 +0100

    changed type checking settings

--- after/plugin/lspconfig.lua
+++ after/plugin/lspconfig.lua
@@ -2,13 +2,16 @@
 local lspconfig = require('lspconfig')
 -- configure pyright
 lspconfig.pyright.setup {
-  capabilities = capabilities,
-  on_attach = on_attach,
-  settings = {
-    python = {
-      analysis = {
-        typeCheckingMode = "off",
-      },
+    capabilities = capabilities,
+    on_attach = on_attach,
+    settings = {
+        python = {
+            analysis = {
+                PythonPlatform = "Windows",
+                strictListInference = true,
+                strictDictionaryInference = true,
+                strictSetInference = true,
+            },
+        },
     },
-  },
 }
