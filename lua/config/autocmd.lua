54b0bf17932b7f60ad6a4fc1cd947f4a2394d201
konjiii <kenji.opdam@gmail.com>
2024-10-08 01:07:32 +0200

    added autoformatting python code using black

---
+++ lua/config/autocmd.lua
@@ -0,0 +1,14 @@
+vim.api.nvim_create_augroup("AutoFormat", {})
+
+-- run black formatter on python script when saving
+vim.api.nvim_create_autocmd(
+    "BufWritePost",
+    {
+        pattern = "*.py",
+        group = "AutoFormat",
+        callback = function()
+            vim.cmd("silent !black --quiet %")
+            vim.cmd("edit")
+        end,
+    }
+)
