dd80fa3dfba7e17f3838f9333cdc5293b08bef99
konjiii <kenji.opdam@gmail.com>
2024-10-16 13:13:23 +0200

    removed rust_analyzer.setup

--- after/plugin/lspconfig.lua
+++ after/plugin/lspconfig.lua
@@ -1,27 +1,31 @@
 -- setup language servers.
-local lspconfig = require('lspconfig')
+local lspconfig = require("lspconfig")
 -- configure pyright
-lspconfig.pyright.setup {
-    capabilities = capabilities,
-    on_attach = on_attach,
-    settings = {
-        python = {
-            analysis = {
-                PythonPlatform = "Linux",
-                -- typeCheckingMode = "off",
-                -- reportGeneralTypeIssues = "none",
-                strictListInference = true,
-                strictDictionaryInference = true,
-                strictSetInference = true,
-            },
-        },
-    },
-}
+lspconfig.pyright.setup({
+	settings = {
+		python = {
+			analysis = {
+				PythonPlatform = "Linux",
+				-- typeCheckingMode = "off",
+				-- reportGeneralTypeIssues = "none",
+				strictListInference = true,
+				strictDictionaryInference = true,
+				strictSetInference = true,
+				autoImportCompletions = true,
+				autoSearchPaths = true,
+				diagnosticMode = "workspace", -- or "openFilesOnly"
+				typeCheckingMode = "basic", -- or "off", "basic", "strict"
+				useLibraryCodeForTypes = true,
+			},
+		},
+	},
+	single_file_support = false, -- Set this to false to disable single file mode
+})
 
 -- rust_analyzer lsp configuration
-lspconfig.rust_analyzer.setup {
-    -- Server-specific settings. See `:help lspconfig-setup`
-    settings = {
-        ['rust-analyzer'] = {},
-    },
-}
+lspconfig.rust_analyzer.setup({
+	-- Server-specific settings. See `:help lspconfig-setup`
+	settings = {
+		["rust-analyzer"] = {},
+	},
+})
