f19573e6e0662c94e3ea9a74f886dd76d7575467
konjiii <kenji.opdam@gmail.com>
2024-11-05 09:35:46 +0100

    format after save instead of on to fix black timeout

--- after/plugin/conform.lua
+++ after/plugin/conform.lua
@@ -1,8 +1,5 @@
 require("conform").setup({
-	format_on_save = {
-		-- These options will be passed to conform.format()
-		timeout_ms = 500,
-	},
+	format_after_save = {},
 	formatters_by_ft = {
 		lua = { "stylua" },
 		python = { "black" },
