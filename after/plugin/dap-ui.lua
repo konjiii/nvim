3c1e195731a7c80af7f2ffeeef33693babe67517
konjiii <kenji.opdam@gmail.com>
2024-11-11 13:41:37 +0100

    added python debugger

--- after/plugin/dap-ui.lua
+++ after/plugin/dap-ui.lua
@@ -8,6 +8,31 @@ dap.adapters.cppdbg = {
 	command = "/home/konji/.local/share/nvim/mason/bin/OpenDebugAD7",
 }
 
+dap.adapters.python = function(cb, config)
+	if config.request == "attach" then
+		---@diagnostic disable-next-line: undefined-field
+		local port = (config.connect or config).port
+		---@diagnostic disable-next-line: undefined-field
+		local host = (config.connect or config).host or "127.0.0.1"
+		cb({
+			type = "server",
+			port = assert(port, "`connect.port` is required for a python `attach` configuration"),
+			host = host,
+			options = {
+				source_filetype = "python",
+			},
+		})
+	else
+		cb({
+			type = "executable",
+			command = "/home/konji/.local/share/nvim/mason/bin/debugpy-adapter",
+			options = {
+				source_filetype = "python",
+			},
+		})
+	end
+end
+
 -- configuration per language
 dap.configurations.c = {
 	{
@@ -20,18 +45,30 @@ dap.configurations.c = {
 		cwd = "${workspaceFolder}",
 		stopAtEntry = true,
 	},
-	-- {
-	-- 	name = "Attach to gdbserver :1234",
-	-- 	type = "cppdbg",
-	-- 	request = "launch",
-	-- 	MIMode = "gdb",
-	-- 	miDebuggerServerAddress = "localhost:1234",
-	-- 	miDebuggerPath = "/usr/bin/gdb",
-	-- 	cwd = "${workspaceFolder}",
-	-- 	program = function()
-	-- 		return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-	-- 	end,
-	-- },
+}
+
+dap.configurations.python = {
+	{
+		-- The first three options are required by nvim-dap
+		type = "python", -- the type here established the link to the adapter definition: `dap.adapters.python`
+		request = "launch",
+		name = "Launch file",
+
+		-- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
+
+		program = "${file}", -- This configuration will launch the current file if used.
+		pythonPath = function()
+			-- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
+			-- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
+			-- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
+			local pyd = os.getenv("VIRTUAL_ENV")
+			if pyd == nil then
+				return "/bin/python"
+			else
+				return pyd .. "/bin/python"
+			end
+		end,
+	},
 }
 
 -- dap keymaps
