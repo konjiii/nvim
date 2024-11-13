aeac283b2e3a38c72de0ef3df956e519b63504fb
konjiii <kenji.opdam@gmail.com>
2024-11-13 03:07:48 +0100

    changed leap bindings

--- after/plugin/leap.lua
+++ after/plugin/leap.lua
@@ -1,6 +1,7 @@
 -- nvim leap keybinds
-vim.keymap.set({ "n", "x", "o" }, "f", "<Plug>(leap)")
-vim.keymap.set({ "n", "x", "o" }, "gf", "<Plug>(leap-from-window)")
+vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap)")
+vim.keymap.set({ "n", "x", "o" }, "gs", "<Plug>(leap-from-window)")
 
 require("leap.user").set_repeat_keys("<enter>", "<backspace>")
 require("leap").opts.equivalence_classes = { " \t\r\n", "([{", ")]}", "'\"`" }
+-- require("leap").opts.safe_labels = {}
