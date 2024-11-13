dc81029b3b1d791133178dbc54024ec4c7099a2e
konjiii <kenji.opdam@gmail.com>
2024-11-13 02:18:28 +0100

    added vim-dadbod

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,43 +1,70 @@
-local lsp_zero = require('lsp-zero')
+local lsp_zero = require("lsp-zero")
 
+-- setup lsp-zero
 lsp_zero.on_attach(function(client, bufnr)
-    local opts = {buffer = bufnr, remap = false}
+	local opts = { buffer = bufnr, remap = false }
 
-    vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
-    vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
-    vim.keymap.set("n", "<leader>gd", function() vim.lsp.buf.definition() end, opts)
-    vim.keymap.set("n", "<leader>gr", function() vim.lsp.buf.references() end, opts)
-    vim.keymap.set("n", "<leader>si", function() vim.lsp.buf.hover() end, opts)
-    vim.keymap.set("n", "<leader>sds", function() vim.lsp.buf.document_symbol() end, opts)
-    vim.keymap.set("n", "<leader>sws", function() vim.lsp.buf.workspace_symbol() end, opts)
-    vim.keymap.set("n", "<leader>se", function() vim.diagnostic.open_float() end, opts)
+	vim.keymap.set("n", "<leader>rn", function()
+		vim.lsp.buf.rename()
+	end, opts)
+	vim.keymap.set("n", "<leader>ca", function()
+		vim.lsp.buf.code_action()
+	end, opts)
+	vim.keymap.set("n", "<leader>gd", function()
+		vim.lsp.buf.definition()
+	end, opts)
+	vim.keymap.set("n", "<leader>gr", function()
+		vim.lsp.buf.references()
+	end, opts)
+	vim.keymap.set("n", "<leader>si", function()
+		vim.lsp.buf.hover()
+	end, opts)
+	vim.keymap.set("n", "<leader>sds", function()
+		vim.lsp.buf.document_symbol()
+	end, opts)
+	vim.keymap.set("n", "<leader>sws", function()
+		vim.lsp.buf.workspace_symbol()
+	end, opts)
+	vim.keymap.set("n", "<leader>se", function()
+		vim.diagnostic.open_float()
+	end, opts)
 end)
 
-require('mason').setup({})
-require('mason-lspconfig').setup({
-    ensure_installed = {'pyright', 'lua_ls', 'clangd'},
-    handlers = {
-        lsp_zero.default_setup,
-        lua_ls = function()
-            local lua_opts = lsp_zero.nvim_lua_ls()
-            require('lspconfig').lua_ls.setup(lua_opts)
-        end,
-    }
+-- setup mason
+require("mason").setup({})
+require("mason-lspconfig").setup({
+	ensure_installed = { "pyright", "lua_ls", "clangd" },
+	handlers = {
+		lsp_zero.default_setup,
+		lua_ls = function()
+			local lua_opts = lsp_zero.nvim_lua_ls()
+			require("lspconfig").lua_ls.setup(lua_opts)
+		end,
+	},
 })
 
-local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
+-- setup nvim-cmp
+local cmp = require("cmp")
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
 cmp.setup({
-    sources = {
-        {name = 'path'},
-        {name = 'nvim_lsp'},
-        {name = 'nvim_lua'},
-    },
-    formatting = lsp_zero.cmp_format(),
-    mapping = cmp.mapping.preset.insert({
-        ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
-        ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
-        ['<Enter>'] = cmp.mapping.confirm({ select = false }),
-    }),
+	sources = {
+		{ name = "path" },
+		{ name = "nvim_lsp" },
+		{ name = "nvim_lua" },
+	},
+	formatting = lsp_zero.cmp_format(),
+	mapping = cmp.mapping.preset.insert({
+		["<C-k>"] = cmp.mapping.select_prev_item(cmp_select),
+		["<C-j>"] = cmp.mapping.select_next_item(cmp_select),
+		["<Enter>"] = cmp.mapping.confirm({ select = false }),
+	}),
+})
+
+-- setup vim-dadbod-completion
+cmp.setup.filetype({ "sql", "mysql", "plsql" }, {
+	sources = {
+		{ name = "vim-dadbod-completion" },
+		{ name = "buffer" },
+	},
 })
