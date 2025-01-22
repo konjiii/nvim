635db789dbb0eca797e8c4868f2ff77618e86091
konjiii <kenji.opdam@gmail.com>
2025-01-22 18:22:35 +0100

    removed pyright lsp settings since pyright didn't work anymore

--- after/plugin/lspconfig.lua
+++ after/plugin/lspconfig.lua
@@ -1,26 +1,26 @@
 -- setup language servers.
 local lspconfig = require("lspconfig")
 -- configure pyright
-lspconfig.pyright.setup({
-	settings = {
-		python = {
-			analysis = {
-				PythonPlatform = "Linux",
-				-- typeCheckingMode = "off",
-				-- reportGeneralTypeIssues = "none",
-				strictListInference = true,
-				strictDictionaryInference = true,
-				strictSetInference = true,
-				autoImportCompletions = true,
-				autoSearchPaths = true,
-				diagnosticMode = "workspace", -- or "openFilesOnly"
-				typeCheckingMode = "basic", -- or "off", "basic", "strict"
-				useLibraryCodeForTypes = true,
-			},
-		},
-	},
-	single_file_support = false, -- Set this to false to disable single file mode
-})
+-- lspconfig.pyright.setup({
+-- 	settings = {
+-- 		python = {
+-- 			analysis = {
+-- 				PythonPlatform = "Linux",
+-- 				-- typeCheckingMode = "off",
+-- 				-- reportGeneralTypeIssues = "none",
+-- 				strictListInference = true,
+-- 				strictDictionaryInference = true,
+-- 				strictSetInference = true,
+-- 				autoImportCompletions = true,
+-- 				autoSearchPaths = true,
+-- 				diagnosticMode = "workspace", -- or "openFilesOnly"
+-- 				typeCheckingMode = "basic", -- or "off", "basic", "strict"
+-- 				useLibraryCodeForTypes = true,
+-- 			},
+-- 		},
+-- 	},
+-- 	single_file_support = false, -- Set this to false to disable single file mode
+-- })
 
 -- rust_analyzer lsp configuration
 lspconfig.rust_analyzer.setup({
